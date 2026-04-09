07cecd78d2d400990bbb4ec98734a75b523d402e
federicosimone <federico.simone15@gmail.com>
2026-04-09 23:06:12 +0200

    snack 2 - quadrato

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// snack 1 
+// snack 1
 
 /*function somma(a, b) {
     return a + b;
@@ -11,6 +11,14 @@
 
 let risultato = somma(2,3)*/
 
-const somma = (a, b) => (a + b)
+/*const somma = (a, b) => (a + b)
 
-let risultato = somma(2, 4)
+let risultato = somma(2, 4)*/
+
+//snack 2 
+
+const squared = (number) => (number * number);
+
+let risultato = squared(9);
+
+console.log(risultato)
