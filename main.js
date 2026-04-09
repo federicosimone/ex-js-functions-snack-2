a6581697a20818175b3101ac49c20df991149106
federicosimone <federico.simone15@gmail.com>
2026-04-09 23:16:50 +0200

    snack 3 con sottrazione

--- main.js
+++ main.js
@@ -15,10 +15,28 @@ let risultato = somma(2,3)*/
 
 let risultato = somma(2, 4)*/
 
-//snack 2 
+//snack 2
 
-const squared = (number) => (number * number);
+/*const squared = (number) => (number * number);
 
 let risultato = squared(9);
 
-console.log(risultato)
+console.log(risultato) ;*/
+
+//snack 3 
+
+const sottrazione = (a, b) => (b - a);
+
+function eseguiOperazione(a, b, operazione) {
+    if (b > a) {
+        return operazione(a, b)
+    } else {
+        console.log("non vogliamo numeri negativi")
+    }
+
+}
+
+let risultato = eseguiOperazione(6, 10, sottrazione);
+
+console.log("Il risultato è ", risultato)
+
