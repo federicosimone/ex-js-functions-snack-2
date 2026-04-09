ed07f5726c58be54c148d178cb146a3656149333
federicosimone <federico.simone15@gmail.com>
2026-04-09 23:03:42 +0200

    arrow function

--- main.js
+++ main.js
@@ -2,7 +2,15 @@
 
 /*function somma(a, b) {
     return a + b;
-}*/
-const somma = function () { console.log(a + b) }
+}
 
-console.log(somma)
+*/
+
+
+/*const somma = function (a,b) { return(a + b) }
+
+let risultato = somma(2,3)*/
+
+const somma = (a, b) => (a + b)
+
+let risultato = somma(2, 4)
